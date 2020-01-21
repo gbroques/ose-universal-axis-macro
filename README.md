d04f6dc61847d3da7748c1f91d3b46660d0717d8
G Roques <groques360@gmail.com>
2020-01-20 22:00:49 -0600

    Add universal axis object consisting of 2 boxes connected by 2 rods with a modifiable Length property

---
+++ README.md
@@ -0,0 +1,12 @@
+# OSE Universal Axis Macro
+The [Universal Axis](https://wiki.opensourceecology.org/wiki/Universal_CNC_Axis) is a modular and scalable CNC axis for creating cartesian CNC machines.
+
+This macro creates a Universal Axis object allowing the user to adjust properties like `Length` automatically.
+
+## Setup
+1. Clone the repository
+2. Link the macro to your FreeCAD macro directory by running the `link_macro.sh` shell script:
+
+```
+./link_macro.sh
+```
---
+++ link_macro.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------------- #
+# Create a symbolic link of UniversalAxis.FCMacro to default FreeCAD macro directory. #
+# ----------------------------------------------------------------------------------- #
+
+set -x  # Print shell command before executing
+set -e  # Exit shell script upon non-zero status code (error) of command
+
+# Create temporary python script to find user's default FreeCAD Macro directory
+touch find_macro_dir.py
+echo "import FreeCAD" >> find_macro_dir.py
+echo "print FreeCAD.getUserMacroDir(True)" >> find_macro_dir.py
+echo "exit(0)" >> find_macro_dir.py
+
+# TODO: Check if freecad command is available before executing
+macro_dir=`freecad -c find_macro_dir.py | tail -1`
+
+# Clean-up temporary python script and .pyc file
+rm find_macro_dir.py*
+
+# Soft link macro from repositiory to user's default FreeCAD macro directory
+ln -s $(pwd)/UniversalAxis.FCMacro $macro_dir/UniversalAxis.FCMacro
+
