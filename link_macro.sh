ec2ab28f3159117de9d4fbb13c97b5643c7404fc
G Roques <groques360@gmail.com>
2020-01-25 19:47:20 -0600

    Correct package name from universalaxis to universal_axis in soft link command

--- link_macro.sh
+++ link_macro.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# ----------------------------------------------------------------------------------- #
-# Create a symbolic link of UniversalAxis.FCMacro to default FreeCAD macro directory. #
-# ----------------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
+# Create a symbolic link of UniversalAxis.FCMacro to FreeCAD macro directory. #
+# --------------------------------------------------------------------------- #
 
 set -x  # Print shell command before executing
 set -e  # Exit shell script upon non-zero status code (error) of command
 
-# Create temporary python script to find user's default FreeCAD Macro directory
+# Create temporary python script to find user's FreeCAD Macro directory
 touch find_macro_dir.py
 echo "import FreeCAD" >> find_macro_dir.py
 echo "print FreeCAD.getUserMacroDir(True)" >> find_macro_dir.py
@@ -18,6 +18,6 @@ macro_dir=`freecad -c find_macro_dir.py | tail -1`
 # Clean-up temporary python script and .pyc file
 rm find_macro_dir.py*
 
-# Soft link macro and package from repositiory to user's default FreeCAD macro directory
+# Soft link macro and package from repositiory to user's FreeCAD macro directory
 ln -s $(pwd)/UniversalAxis.FCMacro $macro_dir/UniversalAxis.FCMacro
-ln -s $(pwd)/universalaxis $macro_dir/universalaxis
+ln -s $(pwd)/universal_axis $macro_dir/universal_axis
